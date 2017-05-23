5db847620009c2b933a40f64dc3b6a7947ca0811
Distroir <radcraftplay@gmail.com>
2017-05-23 15:41:35 +0200

    Added "Profile" and "Template" classes

---
+++ Custom SDK Launcher/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Custom_SDK_Launcher
+{
+    public class Profile
+    {
+        public string ProfileName;
+
+        public string GameDir;
+        public string GameinfoDirName;
+
+        public string GetBinDirectory()
+        {
+            return Path.Combine(GameDir, "bin");
+        }
+
+        public string GetGameinfoDirectory()
+        {
+            return Path.Combine(GameDir, GameinfoDirName);
+        }
+    }
+}
---
+++ Custom SDK Launcher/Template.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Custom_SDK_Launcher
+{
+    class Template
+    {
+        public string Name;
+        public string GameinfoDirName;
+    }
+}
