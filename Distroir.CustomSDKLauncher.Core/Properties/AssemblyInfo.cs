f4438cb6a4840936fb8d0377c50a56b87781a0a6
Distroir <radcraftplay2@gmail.com>
2017-09-30 13:11:44 +0200

    Updated app info

--- Distroir.CustomSDKLauncher.Core/Properties/AssemblyInfo.cs
+++ Distroir.CustomSDKLauncher.Core/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
