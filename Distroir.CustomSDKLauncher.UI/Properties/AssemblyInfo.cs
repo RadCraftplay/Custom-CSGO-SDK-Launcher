ebb8a9df7275adc1d707dac7fa7f6305e51c3fcd
Distroir <radcraftplay2@gmail.com>
2018-02-11 18:01:10 +0100

    Updated copyright

--- Distroir.CustomSDKLauncher.UI/Properties/AssemblyInfo.cs
+++ Distroir.CustomSDKLauncher.UI/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Distroir")]
 [assembly: AssemblyProduct("Distroir.CustomSDKLauncher.UI")]
-[assembly: AssemblyCopyright("Copyright © Distroir 2017")]
+[assembly: AssemblyCopyright("Copyright © Distroir 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
