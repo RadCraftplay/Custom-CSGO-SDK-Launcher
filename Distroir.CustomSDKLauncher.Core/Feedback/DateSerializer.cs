8ad0475eb12b2d4002b50866d9717290fb2e09d8
Distroir <radcraftplay2@gmail.com>
2018-03-28 17:41:14 +0200

    Added feedback scheduler

---
+++ Distroir.CustomSDKLauncher.Core/Feedback/DateSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Feedback
+{
+    public class DateSerializer
+    {
+        public static string SerializeDate(DateTime date)
+        {
+            return $"{date.Year}-{date.Month}-{date.Day}";
+        }
+
+        public static DateTime DeserializeDate(string date)
+        {
+            string[] dataSplitted = date.Split('-');
+            int[] values = new int[3];
+
+            //Read int values
+            for (int i = 0; i < 3; i++)
+                values[i] = Convert.ToInt32(dataSplitted[i]);
+
+            return new DateTime(values[0], values[1], values[2]);
+        }
+    }
+}
---
+++ Distroir.CustomSDKLauncher.Core/Feedback/Scheduler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Feedback
+{
+    public class Scheduler
+    {
+        TimeSpan span;
+
+        public Scheduler(TimeSpan scheduleSpan)
+        {
+            span = scheduleSpan;
+        }
+
+        /// <summary>
+        /// Checks if time passed
+        /// </summary>
+        /// <returns>True if time passed</returns>
+        public bool Check(string dateInPast)
+        {
+            return Check(DateSerializer.DeserializeDate(dateInPast));
+        }
+
+        /// <summary>
+        /// Checks if time passed
+        /// </summary>
+        /// <returns>True if time passed</returns>
+        public bool Check(DateTime dateInPast)
+        {
+            TimeSpan reference = DateTime.Now - dateInPast;
+            return TimeSpan.Compare(span, reference) == -1;
+        }
+
+        
+    }
+}
