b24aa4a7ed306856e31454b0c59d9f754246c89a
Distroir <radcraftplay2@gmail.com>
2018-03-28 17:44:09 +0200

    Revert "Revert "Added feedback scheduler""
    This reverts commit 05328846aa353330fc91f7096d5e7429d24eb877.

--- Distroir.CustomSDKLauncher.Core/Feedback/FeedbackFetcher.cs
+++ Distroir.CustomSDKLauncher.Core/Feedback/FeedbackFetcher.cs
@@ -13,28 +13,59 @@ namespace Distroir.CustomSDKLauncher.Core.Feedback
         string FeedbackQuestion = "Would you like to answer few questions regarding Custom SDK Launcher?\nThis will take less than 5 minutes";
         string MessageTitle = "Feedback notification";
         string SurveyURL = "https://docs.google.com/forms/d/e/1FAIpQLScanK0_P7bHWM9RGHPirBvnIQp_3PoCqXufOz_-umPcPDuYnQ/viewform?usp=sf_link";
+        TimeSpan feedbackDelay = TimeSpan.FromDays(60);
 
         /// <summary>
         /// Activates feedback fetcher
         /// </summary>
         public void Activate()
         {
-            bool asked = false;
+            string lastFetchDate = string.Empty;
 
-            //Check if user has been asked for feedback
-            Config.TryReadBool("AskedForFeedback", out asked);
+            Scheduler s = new Scheduler(feedbackDelay);
 
-            //Show message
-            if (!asked)
+            //Try to get date of previous fetch
+            if (!Config.TryReadString("LastFeedbackFetchDate", out lastFetchDate))
+            {
+                //If variable does not exist, reset date
+                ResetDate(out lastFetchDate);
+                return;
+            }
+
+            //Check if time passed
+            if (s.Check(lastFetchDate))
+            {
+                //Show message
                 ShowMessage();
+                //Reset date
+                ResetDate();
+            }
         }
 
+        /// <summary>
+        /// Resets last fetch date to actual date on machine
+        /// </summary>
+        void ResetDate(out string fetchDate)
+        {
+            fetchDate = DateSerializer.SerializeDate(DateTime.Now);
+            Config.AddVariable("LastFeedbackFetchDate", fetchDate);
+        }
+
+        /// <summary>
+        /// Resets last fetch date to actual date on machine
+        /// </summary>
+        void ResetDate()
+        {
+            Config.AddVariable("LastFeedbackFetchDate", DateSerializer.SerializeDate(DateTime.Now));
+        }
+
+        /// <summary>
+        /// Ask user to participate
+        /// </summary>
         void ShowMessage()
         {
             if (MessageBox.Show(FeedbackQuestion, MessageTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 SendFeedback();
-
-            Config.AddVariable("AskedForFeedback", true);
         }
 
         void SendFeedback()
