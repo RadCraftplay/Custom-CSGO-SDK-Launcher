f8b613c749c0228dc35f60486368e995da987e84
Distroir <radcraftplay2@gmail.com>
2019-07-14 11:45:43 +0200

    Removed unused usings in HelpTopic.cs

--- Distroir.CustomSDKLauncher.Shared/Core/HelpTopic.cs
+++ Distroir.CustomSDKLauncher.Shared/Core/HelpTopic.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Distroir.CustomSDKLauncher.Shared.Core
+﻿namespace Distroir.CustomSDKLauncher.Shared.Core
 {
     public class HelpTopic
     {
