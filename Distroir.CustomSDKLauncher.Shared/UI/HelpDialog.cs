6594610eed438ced0723d5eb0be5111aa18da364
Distroir <radcraftplay2@gmail.com>
2019-07-14 11:45:10 +0200

    Added HelpDialog

---
+++ Distroir.CustomSDKLauncher.Shared/UI/HelpDialog.cs
@@ -0,0 +1,20 @@
+﻿using Distroir.CustomSDKLauncher.Shared.Core;
+using System.Windows.Forms;
+
+namespace Distroir.CustomSDKLauncher.Shared.UI
+{
+    public partial class HelpDialog : Form
+    {
+        public HelpDialog(HelpTopic topic)
+        {
+            InitializeComponent();
+            UpdateUI(topic);
+        }
+
+        private void UpdateUI(HelpTopic topic)
+        {
+            topicNameLabel.Text = topic.Name;
+            topicTextRichTextBox.Text = topic.Text;
+        }
+    }
+}
