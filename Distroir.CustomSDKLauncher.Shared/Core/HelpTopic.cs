88c4a22b03cbfdb87791b1d0d681b79617daa714
Distroir <radcraftplay2@gmail.com>
2019-07-14 11:33:59 +0200

    Added HelpTopic class

---
+++ Distroir.CustomSDKLauncher.Shared/Core/HelpTopic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Shared.Core
+{
+    public class HelpTopic
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
