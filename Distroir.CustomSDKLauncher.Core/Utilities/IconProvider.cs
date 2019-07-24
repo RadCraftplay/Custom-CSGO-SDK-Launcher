c3f9ec1b7ca0e2d3c88521bc1396123a6f025b34
Distroir <radcraftplay2@gmail.com>
2019-07-24 17:09:11 +0200

    Added IconProvider.GetIconFromFile method

--- Distroir.CustomSDKLauncher.Core/Utilities/IconProvider.cs
+++ Distroir.CustomSDKLauncher.Core/Utilities/IconProvider.cs
@@ -15,7 +15,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using Etier.IconHelper;
 
 namespace Distroir.CustomSDKLauncher.Core.Utilities
@@ -51,6 +55,46 @@ namespace Distroir.CustomSDKLauncher.Core.Utilities
             {
                 return Data.DefaultIcon;
             }
+        }
+
+        public Image GetIconFromFile(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException(nameof(path));
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File with name '{path}' not found!");
+
+            Image icon = Image.FromFile(path);
+            int iconSize = IconSize == IconReader.IconSize.Small ?
+                16 : 32;
+
+            if (icon.Width != iconSize || icon.Height != iconSize)
+                icon = ResizeIcon(icon, iconSize);
+
+            return icon;
+        }
+
+        private Image ResizeIcon(Image icon, int iconSize)
+        {
+            Rectangle finalRectangle = new Rectangle(0, 0, iconSize, iconSize);
+            Bitmap finalBitmap = new Bitmap(iconSize, iconSize);
+
+            using (Graphics graphics = Graphics.FromImage(finalBitmap))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (ImageAttributes attributes = new ImageAttributes())
+                {
+                    attributes.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(icon, finalRectangle, 0, 0, icon.Width, icon.Height, GraphicsUnit.Pixel, attributes);
+                }
+            }
+
+            return finalBitmap;
         }
     }
 }
