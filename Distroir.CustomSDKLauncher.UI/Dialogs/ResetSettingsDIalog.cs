f21ea6eb0f0474b1bc83ff8e3b9839ed5799eb26
Distroir <radcraftplay2@gmail.com>
2019-08-31 15:46:38 +0200

    Added ResetSettingsDialog

---
+++ Distroir.CustomSDKLauncher.UI/Dialogs/ResetSettingsDIalog.cs
@@ -0,0 +1,50 @@
+﻿using Distroir.CustomSDKLauncher.Core.Managers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Distroir.CustomSDKLauncher.UI.Dialogs
+{
+    public partial class ResetSettingsDIalog : Form
+    {
+        public ResetSettingsDIalog()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            DialogResult agreement = AskToAgree();
+
+            if (agreement == DialogResult.Yes)
+            {
+                if (gamesCheckBox.Checked)
+                    File.Delete(DataManagers.GameListFilename);
+
+                if (launcherCheckBox.Checked)
+                    File.Delete(DataManagers.AppListFilename);
+
+                if (otherCheckBox.Checked)
+                    File.Delete(Configuration.Config.destination);
+
+                Environment.Exit(0);
+            }
+        }
+
+        private DialogResult AskToAgree()
+        {
+            return MessageBox.Show("Selected settings will be reset. This can not be undone!\nAre you sure you want to continue?", "Resetting settings", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        }
+    }
+}
