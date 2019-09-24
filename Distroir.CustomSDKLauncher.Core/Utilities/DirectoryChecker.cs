860b24dd676c62ac4210fe824447838fda145e39
Distroir <radcraftplay2@gmail.com>
2019-09-24 19:47:06 +0200

    Added DirectoryChecker

---
+++ Distroir.CustomSDKLauncher.Core/Utilities/DirectoryChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Distroir.CustomSDKLauncher.Core.Utilities
+{
+    public class DirectoryChecker : IChecker
+    {
+        public string LastErrorMessage { get; private set; }
+        private readonly Game _gameToCheck;
+
+        public DirectoryChecker(Game toCheck)
+        {
+            _gameToCheck = toCheck;
+        }
+
+        public bool Validate()
+        {
+            string gameInfoDirPath = Path.Combine(_gameToCheck.GameDir, _gameToCheck.GameinfoDirName);
+
+            return Directory.Exists(_gameToCheck.GameDir)
+                && Directory.Exists(gameInfoDirPath);
+        }
+    }
+}
