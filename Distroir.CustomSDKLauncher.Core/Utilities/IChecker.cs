2adcdea8ea6b597f829430fea9a74d5d1269a543
Distroir <radcraftplay2@gmail.com>
2019-09-24 18:22:15 +0200

    Added IChecker interface

---
+++ Distroir.CustomSDKLauncher.Core/Utilities/IChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Utilities
+{
+    public interface IChecker
+    {
+        string LastErrorMessage { get; }
+        bool Validate();
+    }
+}
