95bda263bc26abcbd04d793108112b46cc901880
Distroir <radcraftplay2@gmail.com>
2019-09-24 15:28:26 +0200

    Added GameChecker

---
+++ Distroir.CustomSDKLauncher.Core/Utilities/GameChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Utilities
+{
+    public class GameChecker
+    {
+        private readonly Game _gameToCheck;
+        private readonly ToolChecker _toolChecker;
+
+        public GameChecker(Game gameToCheck)
+        {
+            _gameToCheck = gameToCheck;
+            _toolChecker = new ToolChecker(_gameToCheck.GameDir);
+        }
+
+        public bool IsValid()
+        {
+            return ValidateDirectories()
+                && ValidateToolPaths();
+        }
+
+        private bool ValidateToolPaths()
+        {
+            return _toolChecker.CheckIfToolsExist();
+        }
+
+        private bool ValidateDirectories()
+        {
+            string gameInfoDirPath = Path.Combine(_gameToCheck.GameDir, _gameToCheck.GameinfoDirName);
+
+            return Directory.Exists(_gameToCheck.GameDir)
+                && Directory.Exists(gameInfoDirPath);
+        }
+    }
+}
