89f2501922156e62e835469d7493adb2d8c892bf
Distroir <radcraftplay2@gmail.com>
2019-09-25 14:04:46 +0200

    Added EmptyValueChecker

---
+++ Distroir.CustomSDKLauncher.Core/Utilities/Checker/EmptyValueChecker.cs
@@ -0,0 +1,25 @@
+﻿namespace Distroir.CustomSDKLauncher.Core.Utilities.Checker
+{
+    class EmptyValueChecker : IChecker
+    {
+        public string LastErrorMessage { get; private set; }
+        private Game _gameToCheck;
+
+        public EmptyValueChecker(Game toCheck)
+        {
+            _gameToCheck = toCheck;
+        }
+
+        public bool Validate()
+        {
+            bool invalid = string.IsNullOrWhiteSpace(_gameToCheck.GameDir)
+                        && string.IsNullOrWhiteSpace(_gameToCheck.GameinfoDirName)
+                        && string.IsNullOrWhiteSpace(_gameToCheck.Name);
+
+            if (invalid)
+                LastErrorMessage = "One or more fields are empty";
+
+            return !invalid;
+        }
+    }
+}
