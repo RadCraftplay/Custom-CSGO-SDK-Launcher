9b9072a80499efcf65f3815806b9d4c48db80817
Distroir <radcraftplay2@gmail.com>
2019-10-02 12:22:22 +0200

    Added IDisplayableItem interface

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/View/IDisplayableItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.View
+{
+    public interface IDisplayableItem
+    {
+        string Name { get; }
+        Image Icon { get; }
+    }
+}
---
+++ Distroir.CustomSDKLauncher.Core/Launchers/View/StandardDisplayableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.View
+{
+    class StandardDisplayableItem : IDisplayableItem
+    {
+        public string Name => throw new NotImplementedException();
+
+        public Image Icon => throw new NotImplementedException();
+    }
+}
