973bb3597a1fffa0c29cab09fa80cda60e6b7904
Distroir <radcraftplay2@gmail.com>
2019-10-02 12:31:38 +0200

    Added StandardDisplayableItem

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/StandardSdkApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers
+{
+    public enum StandardSdkApplication
+    {
+        /// <summary>
+        /// Hammer world editor
+        /// </summary>
+        Hammer,
+        /// <summary>
+        /// Model viewer
+        /// </summary>
+        HLMV,
+        /// <summary>
+        /// Face poser
+        /// </summary>
+        FacePoser
+    }
+}
--- Distroir.CustomSDKLauncher.Core/Launchers/View/StandardDisplayableItem.cs
+++ Distroir.CustomSDKLauncher.Core/Launchers/View/StandardDisplayableItem.cs
@@ -8,8 +8,45 @@ namespace Distroir.CustomSDKLauncher.Core.Launchers.View
 {
     class StandardDisplayableItem : IDisplayableItem
     {
-        public string Name => throw new NotImplementedException();
+        private readonly StandardSdkApplication _application;
 
-        public Image Icon => throw new NotImplementedException();
+        public StandardDisplayableItem(StandardSdkApplication application)
+        {
+            _application = application;
+        }
+
+        public string Name => GetApplicationName(_application);
+
+        public Image Icon => GetApplicationIcon(_application);
+
+        private string GetApplicationName(StandardSdkApplication application)
+        {
+            switch (application)
+            {
+                case StandardSdkApplication.Hammer:
+                    return "Hammer World Editor";
+                case StandardSdkApplication.HLMV:
+                    return "Model Viewer";
+                case StandardSdkApplication.FacePoser:
+                    return "Face poser";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private Image GetApplicationIcon(StandardSdkApplication application)
+        {
+            switch (application)
+            {
+                case StandardSdkApplication.Hammer:
+                    return Data.HammerIcon;
+                case StandardSdkApplication.HLMV:
+                    return Data.ModelViewerIcon;
+                case StandardSdkApplication.FacePoser:
+                    return Data.FacePoserIcon;
+                default:
+                    return null;
+            }
+        }
     }
 }
