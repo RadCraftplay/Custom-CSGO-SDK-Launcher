a2269a8d300a6e2b225eb5235c3c4f66ff8766c7
Distroir <radcraftplay2@gmail.com>
2019-10-02 12:40:05 +0200

    Added AppDependentDisplayableItem

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/View/AppDependentDisplayableItem.cs
@@ -0,0 +1,23 @@
+﻿using Distroir.CustomSDKLauncher.Core.AppLauncher;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.View
+{
+    class AppDependentDisplayableItem : IDisplayableItem
+    {
+        private readonly AppInfo _associatedInfo;
+
+        public AppDependentDisplayableItem(AppInfo associatedInfo)
+        {
+            _associatedInfo = associatedInfo ?? throw new ArgumentNullException(nameof(associatedInfo));
+        }
+
+        public string Name => _associatedInfo.DisplayText;
+
+        public Image Icon => _associatedInfo.Icon;
+    }
+}
