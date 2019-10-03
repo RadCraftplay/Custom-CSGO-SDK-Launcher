a1d9721bea8548006fbcad500e83369bf2c3ffd3
Distroir <radcraftplay2@gmail.com>
2019-10-03 14:31:40 +0200

    Added class Launcher

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Launcher.cs
@@ -0,0 +1,15 @@
+﻿using Distroir.CustomSDKLauncher.Core.Launchers.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers
+{
+    public abstract class Launcher
+    {
+        public abstract List<IDisplayableItem> DisplayableItems { get; }
+
+        public abstract void Launch(int appId);
+    }
+}
