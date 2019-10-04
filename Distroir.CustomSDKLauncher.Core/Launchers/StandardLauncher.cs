1d89a2203ed07993f522c0e778f858b78bed0c5f
Distroir <radcraftplay2@gmail.com>
2019-10-04 21:00:04 +0200

    Added standard launcher

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/StandardLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Distroir.CustomSDKLauncher.Core.Launchers.Apps;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers
+{
+    public class StandardLauncher : Launcher
+    {
+        private readonly List<IApp> _apps;
+
+        public override List<IApp> Apps => _apps;
+
+        public StandardLauncher()
+        {
+            _apps = new List<IApp>()
+            {
+                new StandardApp(StandardSdkApplication.Hammer),
+                new StandardApp(StandardSdkApplication.HLMV),
+                new StandardApp(StandardSdkApplication.FacePoser)
+            };
+        }
+    }
+}
