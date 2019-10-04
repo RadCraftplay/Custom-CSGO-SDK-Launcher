a4d03561485b0017f3d8b76d229b7ecc627e7b6c
Distroir <radcraftplay2@gmail.com>
2019-10-04 20:51:55 +0200

    Added class "Launcher"

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Launcher.cs
@@ -0,0 +1,29 @@
+﻿using Distroir.CustomSDKLauncher.Core.Launchers.Apps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers
+{
+    public abstract class Launcher
+    {
+        public abstract List<IApp> Apps { get; }
+
+        /// <summary>
+        /// Launches application with selected id
+        /// </summary>
+        /// <param name="applicationId">Id of an application (between 0 and 2)</param>
+        /// <param name="activeGame">Currently active game</param>
+        public void Launch(int applicationId, Game activeGame)
+        {
+            if (!Enumerable.Range(0, 2).Contains(applicationId))
+                throw new ArgumentOutOfRangeException("Application id has to be a number between 0 and two");
+
+            if (Apps.Count != 3)
+                throw new IndexOutOfRangeException("Launcher has to have exactly three applications");
+
+            IApp selectedApp = Apps[applicationId];
+            selectedApp.Launch(activeGame);
+        }
+    }
+}
