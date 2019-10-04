216fa1ce914b44b3cb68c82acbef9f42e10863cc
Distroir <radcraftplay2@gmail.com>
2019-10-04 20:41:37 +0200

    Added CustomizableApp class

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Apps/CustomizableApp.cs
@@ -0,0 +1,22 @@
+﻿using Distroir.CustomSDKLauncher.Core.AppLauncher;
+using Distroir.CustomSDKLauncher.Core.Launchers.View;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.Apps
+{
+    public class CustomizableApp : IApp
+    {
+        private readonly AppInfo _info;
+
+        public IDisplayableItem DisplayableItem => new AppDependentDisplayableItem(_info);
+
+        public CustomizableApp(AppInfo info)
+        {
+            _info = info;
+        }
+
+        public void Launch(Game associatedGame)
+        {
+            _info.Launch();
+        }
+    }
+}
