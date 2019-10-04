c1b8f62ba6285d7f2ab95211d17e7cf3259cc2c4
Distroir <radcraftplay2@gmail.com>
2019-10-04 16:57:14 +0200

    Added IApp interface

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Apps/IApp.cs
@@ -0,0 +1,14 @@
+﻿using Distroir.CustomSDKLauncher.Core.Launchers.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.Apps
+{
+    public interface IApp
+    {
+        IDisplayableItem DisplayableItem { get; }
+        void Launch(Game associatedGame);
+    }
+}
