93e15cc632f8ae061443614a1b35beeb24fadd75
RadCraftplay <radcraftplay2@gmail.com>
2020-02-13 21:29:37 +0100

    Added AppManifestReader and LibraryFileReader

---
+++ Distroir.CustomSDKLauncher.Core/Steam/AppManifestReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Gameloop.Vdf;
+using Gameloop.Vdf.Linq;
+
+namespace Distroir.CustomSDKLauncher.Core.Steam
+{
+    public class AppManifestReader : IDisposable
+    {
+        private readonly TextReader _manifestFileReader;
+        
+        public AppManifestReader(string manifestFilename)
+        {
+            _manifestFileReader = new StreamReader(manifestFilename);
+        }
+
+        public string GetRelativeInstallDirectory()
+        {
+            var manifest = VdfConvert.Deserialize(_manifestFileReader.ReadToEnd());
+            var children = manifest.Value.Children();
+
+            return children.FirstOrDefault(x => x.Key == "installdir")?.Value?.ToString();
+        }
+
+        public Template GetGameTemplate()
+        {
+            var manifest = VdfConvert.Deserialize(_manifestFileReader.ReadToEnd());
+            var dictionary = manifest.Value.Children()?
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            return CreateGameTemplate(dictionary);
+        }
+
+        private Template CreateGameTemplate(Dictionary<string, VToken> manifestProperties)
+        {
+            return new Template()
+            {
+                Name = manifestProperties?["name"].ToString(),
+                GameDirName = manifestProperties?["installdir"].ToString()
+            };
+        }
+
+        public void Dispose()
+        {
+            _manifestFileReader?.Dispose();
+        }
+    }
+}
---
+++ Distroir.CustomSDKLauncher.Core/Steam/LibraryFileReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Gameloop.Vdf;
+
+namespace Distroir.CustomSDKLauncher.Core.Steam
+{
+    public class LibraryFileReader : IDisposable
+    {
+        private readonly TextReader _libraryListFileReader;
+
+        public LibraryFileReader(string libraryListFileName)
+        {
+            _libraryListFileReader = new StreamReader(libraryListFileName);
+        }
+
+        public IEnumerable<string> GetLibraryFolders()
+        {
+            var currentIndex = 1;
+            var libraryDeserialized = VdfConvert.Deserialize(_libraryListFileReader.ReadToEnd());
+
+            foreach (var property in libraryDeserialized.Value.Children())
+            {
+                if (property.Key == currentIndex.ToString())
+                {
+                    yield return property.Value.ToString();
+                    currentIndex++;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _libraryListFileReader?.Dispose();
+        }
+    }
+}
