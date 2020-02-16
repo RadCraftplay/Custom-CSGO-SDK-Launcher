71c2b4cccf263e3d2961c32d31e02d1468203e29
RadCraftplay <radcraftplay2@gmail.com>
2020-02-16 21:11:23 +0100

    Added JsonFileSerializer

---
+++ Distroir.CustomSDKLauncher.Core/Managers/Serializers/JsonFileSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Distroir.CustomSDKLauncher.Core.Managers.Serializers
+{
+    public class JsonFileSerializer<T> : ContentSerializer<T>
+    {
+        private readonly string _filename;
+        private readonly JsonSerializer _fileSerializer;
+
+        public JsonFileSerializer(string filename)
+        {
+            CanSave = true;
+            _filename = filename;
+            _fileSerializer = new JsonSerializer()
+            {
+                TypeNameHandling = TypeNameHandling.Objects,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+                Formatting = Formatting.Indented
+            };
+        }
+
+        public override T[] Load()
+        {
+            using (TextReader reader = new StreamReader(_filename))
+            using (JsonTextReader jsonReader = new JsonTextReader(reader))
+                return _fileSerializer.Deserialize<T[]>(jsonReader);
+        }
+
+        public override void Save(T[] array)
+        {
+            using (TextWriter writer = new StreamWriter(_filename))
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(writer))
+                _fileSerializer.Serialize(jsonWriter, array);
+        }
+    }
+}
