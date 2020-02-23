71bbb77f0298a36dd5acd4505dddb47e68a65511
RadCraftplay <radcraftplay2@gmail.com>
2020-02-23 15:28:05 +0100

    Fixed #95

--- Distroir.CustomSDKLauncher.UI/Dialogs/CommunityContentBrowserDialog.Designer.cs
+++ Distroir.CustomSDKLauncher.UI/Dialogs/CommunityContentBrowserDialog.Designer.cs
@@ -29,8 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources =
-                new System.ComponentModel.ComponentResourceManager(typeof(CommunityContentBrowserDialog));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CommunityContentBrowserDialog));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.contentTreeView = new System.Windows.Forms.TreeView();
             this.goButton = new System.Windows.Forms.Button();
@@ -38,7 +37,7 @@
             this.contentAuthorLabel = new System.Windows.Forms.Label();
             this.contentNameLabel = new System.Windows.Forms.Label();
             this.groupImageList = new System.Windows.Forms.ImageList(this.components);
-            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -48,7 +47,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -61,8 +60,8 @@
             this.splitContainer1.Panel2.Controls.Add(this.descriptionTextBox);
             this.splitContainer1.Panel2.Controls.Add(this.contentAuthorLabel);
             this.splitContainer1.Panel2.Controls.Add(this.contentNameLabel);
-            this.splitContainer1.Size = new System.Drawing.Size(700, 422);
-            this.splitContainer1.SplitterDistance = 157;
+            this.splitContainer1.Size = new System.Drawing.Size(600, 366);
+            this.splitContainer1.SplitterDistance = 134;
             this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 0;
             // 
@@ -70,26 +69,21 @@
             // 
             this.contentTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.contentTreeView.Location = new System.Drawing.Point(0, 0);
-            this.contentTreeView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.contentTreeView.Margin = new System.Windows.Forms.Padding(2);
             this.contentTreeView.Name = "contentTreeView";
-            this.contentTreeView.Size = new System.Drawing.Size(157, 422);
+            this.contentTreeView.Size = new System.Drawing.Size(134, 366);
             this.contentTreeView.TabIndex = 0;
-            this.contentTreeView.AfterSelect +=
-                new System.Windows.Forms.TreeViewEventHandler(this.contentTreeView_AfterSelect);
-            this.contentTreeView.NodeMouseClick +=
-                new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.contentTreeView_NodeMouseClick);
-            this.contentTreeView.NodeMouseDoubleClick +=
-                new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.contentTreeView_NodeMouseClick);
+            this.contentTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.contentTreeView_AfterSelect);
+            this.contentTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.contentTreeView_NodeMouseClick);
+            this.contentTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.contentTreeView_NodeMouseClick);
             // 
             // goButton
             // 
-            this.goButton.Anchor =
-                ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top |
-                                                       System.Windows.Forms.AnchorStyles.Right)));
-            this.goButton.Location = new System.Drawing.Point(491, 8);
-            this.goButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.goButton.Location = new System.Drawing.Point(422, 7);
+            this.goButton.Margin = new System.Windows.Forms.Padding(2);
             this.goButton.Name = "goButton";
-            this.goButton.Size = new System.Drawing.Size(38, 42);
+            this.goButton.Size = new System.Drawing.Size(33, 36);
             this.goButton.TabIndex = 2;
             this.goButton.Text = "Go";
             this.goButton.UseVisualStyleBackColor = true;
@@ -98,79 +92,70 @@
             // 
             // descriptionTextBox
             // 
-            this.descriptionTextBox.Anchor =
-                ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top |
-                                                         System.Windows.Forms.AnchorStyles.Bottom) |
-                                                        System.Windows.Forms.AnchorStyles.Left) |
-                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.descriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.descriptionTextBox.BackColor = System.Drawing.SystemColors.Control;
             this.descriptionTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.descriptionTextBox.Font = new System.Drawing.Font("Calibri", 10.1F, System.Drawing.FontStyle.Regular,
-                System.Drawing.GraphicsUnit.Point, ((byte) (238)));
-            this.descriptionTextBox.Location = new System.Drawing.Point(6, 58);
-            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.descriptionTextBox.Font = new System.Drawing.Font("Calibri", 10.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(5, 50);
+            this.descriptionTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.descriptionTextBox.Name = "descriptionTextBox";
             this.descriptionTextBox.ReadOnly = true;
-            this.descriptionTextBox.Size = new System.Drawing.Size(524, 353);
+            this.descriptionTextBox.Size = new System.Drawing.Size(447, 305);
             this.descriptionTextBox.TabIndex = 2;
             this.descriptionTextBox.Text = "Click on one of items on left side of the control to get more info";
             this.descriptionTextBox.Enter += new System.EventHandler(this.descriptionTextBox_Enter);
             // 
             // contentAuthorLabel
             // 
-            this.contentAuthorLabel.Anchor =
-                ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top |
-                                                        System.Windows.Forms.AnchorStyles.Left) |
-                                                       System.Windows.Forms.AnchorStyles.Right)));
-            this.contentAuthorLabel.Font = new System.Drawing.Font("Calibri Light", 10.2F,
-                System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (238)));
+            this.contentAuthorLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contentAuthorLabel.Font = new System.Drawing.Font("Calibri Light", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.contentAuthorLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.contentAuthorLabel.Location = new System.Drawing.Point(2, 32);
+            this.contentAuthorLabel.Location = new System.Drawing.Point(2, 28);
             this.contentAuthorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.contentAuthorLabel.Name = "contentAuthorLabel";
-            this.contentAuthorLabel.Size = new System.Drawing.Size(483, 22);
+            this.contentAuthorLabel.Size = new System.Drawing.Size(415, 19);
             this.contentAuthorLabel.TabIndex = 1;
             this.contentAuthorLabel.Text = "N/A | Unknown";
             // 
             // contentNameLabel
             // 
-            this.contentNameLabel.Anchor =
-                ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top |
-                                                        System.Windows.Forms.AnchorStyles.Left) |
-                                                       System.Windows.Forms.AnchorStyles.Right)));
-            this.contentNameLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold,
-                System.Drawing.GraphicsUnit.Point, ((byte) (238)));
-            this.contentNameLabel.Location = new System.Drawing.Point(2, 8);
+            this.contentNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contentNameLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.contentNameLabel.Location = new System.Drawing.Point(2, 7);
             this.contentNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.contentNameLabel.Name = "contentNameLabel";
-            this.contentNameLabel.Size = new System.Drawing.Size(483, 23);
+            this.contentNameLabel.Size = new System.Drawing.Size(415, 20);
             this.contentNameLabel.TabIndex = 0;
             this.contentNameLabel.Text = "Community content gallery";
             // 
             // groupImageList
             // 
-            this.groupImageList.ImageStream =
-                ((System.Windows.Forms.ImageListStreamer) (resources.GetObject("groupImageList.ImageStream")));
+            this.groupImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("groupImageList.ImageStream")));
             this.groupImageList.TransparentColor = System.Drawing.Color.Transparent;
             this.groupImageList.Images.SetKeyName(0, "blank.png");
             // 
             // CommunityContentBrowserDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 422);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CommunityContentBrowserDialog";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Community Content Gallery";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         #endregion
