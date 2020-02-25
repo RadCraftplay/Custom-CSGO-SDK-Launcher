69c650e7f14df9a2743e710ad71c38f2e09ec320
RadCraftplay <radcraftplay2@gmail.com>
2020-02-25 14:00:15 +0100

    Added JsonIgnore tag in IApp.cs

--- Distroir.CustomSDKLauncher.Core/Launchers/IApp.cs
+++ Distroir.CustomSDKLauncher.Core/Launchers/IApp.cs
@@ -16,10 +16,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using Newtonsoft.Json;
+
 namespace Distroir.CustomSDKLauncher.Core.Launchers
 {
     public interface IApp
     {
+        [JsonIgnore]
         IDisplayableItem DisplayableItem { get; }
         void Launch(Game associatedGame);
     }
