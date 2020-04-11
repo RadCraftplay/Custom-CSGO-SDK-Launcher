c0c534e1daf98590b1f3cb435c88c137bbe54068
RadCraftplay <radcraftplay2@gmail.com>
2020-04-11 17:39:12 +0200

    Added equality members of JavaAppTemplate class

--- Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Templates/JavaAppTemplate.cs
+++ Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Templates/JavaAppTemplate.cs
@@ -16,19 +16,30 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using System;
 using Distroir.CustomSDKLauncher.Core.AppLauncher.Dialogs;
 
 namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Templates
 {
-    public class JavaAppTemplate : AppTemplate
+    public class JavaAppTemplate : AppTemplate, IEquatable<JavaAppTemplate>
     {
         public override AppInfo Info => _info;
 
-        private AppInfo _info = new AppInfo
+        private AppInfo _info;
+
+        public JavaAppTemplate()
         {
-            Icon = Data.DefaultIcon,
-            DisplayText = "Java application"
-        };
+            _info = new AppInfo()
+            {
+                Icon = Data.DefaultIcon,
+                DisplayText = "Java application"
+            };
+        }
+
+        public JavaAppTemplate(AppInfo info)
+        {
+            _info = info;
+        }
 
         public override bool Configure()
         {
@@ -40,6 +51,36 @@ namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Tem
             }
 
             return false;
+        }
+        
+        public bool Equals(JavaAppTemplate other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(_info, other._info);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((JavaAppTemplate) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_info != null ? _info.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(JavaAppTemplate left, JavaAppTemplate right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(JavaAppTemplate left, JavaAppTemplate right)
+        {
+            return !Equals(left, right);
         }
     }
 }
