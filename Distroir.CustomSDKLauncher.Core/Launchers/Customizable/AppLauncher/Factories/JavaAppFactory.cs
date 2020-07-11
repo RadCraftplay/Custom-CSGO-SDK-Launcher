8fb8106c55c90ee230643f4bcc42d0ff7a85144a
RadCraftplay <radcraftplay2@gmail.com>
2020-07-11 18:58:47 +0200

    Added JavaAppFactory based on JavaAppTemplate

---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Factories/JavaAppFactory.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Factories
+{
+    public class JavaAppFactory : IAppInfoFactory
+    {
+        public JavaAppFactory(JavaApplication application)
+        {
+            Application = application ?? throw new ArgumentNullException(nameof(application));
+        }
+        
+        public JavaApplication Application { get; }
+        
+        public AppInfo GetInfo()
+        {
+            return new AppInfo()
+            {
+                DisplayText = Application.Name,
+                Icon = Application.Icon,
+                Path = Application.JarFilePath,
+                UseCustomArguments = true,
+                Arguments = $"-jar {'"'}{Application.JarFilePath}{'"'}"
+            };
+        }
+    }
+}
---
+++ Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Factories/JavaApplication.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Drawing;
+
+namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Factories
+{
+    public class JavaApplication
+    {
+        public JavaApplication(string name, string jarFilePath, string javaExecutablePath, Image icon)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            JarFilePath = jarFilePath ?? throw new ArgumentNullException(nameof(jarFilePath));
+            JavaExecutablePath = javaExecutablePath ?? throw new ArgumentNullException(nameof(javaExecutablePath));
+            Icon = icon ?? throw new ArgumentNullException(nameof(icon));
+        }
+
+        public string Name { get; }
+        public string JarFilePath { get; }
+        public string JavaExecutablePath { get; }
+        public Image Icon { get; }
+    }
+}
