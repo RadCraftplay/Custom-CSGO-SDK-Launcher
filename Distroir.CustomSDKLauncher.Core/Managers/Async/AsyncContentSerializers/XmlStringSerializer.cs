53ee8844286cf05e0adaea5d13c14b2f38536555
RadCraftplay <radcraftplay2@gmail.com>
2020-07-31 12:09:10 +0200

    Added XmlStringSerializer

---
+++ Distroir.CustomSDKLauncher.Core/Managers/Async/AsyncContentSerializers/XmlStringSerializer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Distroir.CustomSDKLauncher.Core.Managers.Async.AsyncContentSerializers
+{
+    public class XmlStringSerializer<T> : ISerializer<T>
+    {
+        private readonly string _toDeserialize;
+
+        public XmlStringSerializer(string toDeserialize)
+        {
+            _toDeserialize = toDeserialize;
+        }
+        
+        public void Serialize(T toSerialize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize()
+        {
+            using (TextReader reader = new StringReader(_toDeserialize))
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
