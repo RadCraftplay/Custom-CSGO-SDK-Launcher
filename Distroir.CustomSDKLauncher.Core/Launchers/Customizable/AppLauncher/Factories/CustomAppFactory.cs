865d43dd00a8ec84042105c87e3793af034802de
RadCraftplay <radcraftplay2@gmail.com>
2020-08-11 12:30:01 +0200

    Added Configure method in CustomAppFactory.cs

--- Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Factories/CustomAppFactory.cs
+++ Distroir.CustomSDKLauncher.Core/Launchers/Customizable/AppLauncher/Factories/CustomAppFactory.cs
@@ -1,3 +1,6 @@
+using System.Windows.Forms;
+using Distroir.CustomSDKLauncher.Core.AppLauncher.Dialogs;
+
 namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Factories
 {
     public class CustomAppFactory : IAppInfoFactory
@@ -7,11 +10,24 @@ namespace Distroir.CustomSDKLauncher.Core.Launchers.Customizable.AppLauncher.Fac
             Info = info;
         }
 
-        public AppInfo Info { get; }
+        public AppInfo Info { get; private set; }
 
         public AppInfo GetInfo()
         {
             return Info;
+        }
+
+        public bool Configure()
+        {
+            var dialog = new CustomAppConfigurationDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Info = dialog.info;
+                return true;
+            }
+
+            return false;
         }
     }
 }
